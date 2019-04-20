9d6a26ebb6cbe2c11ee04bdfaa81ba844d0509c9
sergi <sergi@192.168.1.107>
2019-04-20 20:08:11 +0200

    "Versió 0.2 del projecte prjava01"

--- prjava01/src/prjava01/Prjava01.java
+++ prjava01/src/prjava01/Prjava01.java
@@ -9,13 +9,40 @@ package prjava01;
  *
  * @author sergi
  */
+import java.io.*;
+
 public class Prjava01 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write("  <head>");
+            bw.newLine();
+            bw.write("    <title>");
+            bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");
+            bw.newLine();
+            bw.write("    </title>");
+            bw.newLine();
+            bw.write("  </head>");
+            bw.newLine();
+            bw.write("  <body>");
+            bw.newLine();
+            bw.write("    <h1>DAW2 m08uf4pr2</h1>");
+            bw.newLine();
+            bw.write("    Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write("  </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
     }
-    
 }
